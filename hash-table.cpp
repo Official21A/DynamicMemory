2dd354e5c41cd825a2f17a7c8c706fb74bc49eb4
Official21A <officialamirhossein21@gmail.com>
2020-11-26 08:32:02 +0330

    start

---
+++ hash-table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Create an array of pointer as slots
+    // Create a table to generate a key
+    // Create a linked list for any slots to point at
+    // Add the methods
+    // Create the diagram of data structure
+}
