808e939055f61cbac3474df0c1e23e4a1bee2ad4
Official21A <officialamirhossein21@gmail.com>
2020-11-29 08:19:57 +0330

    refactor

--- hash-table.cpp
+++ hash-table.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// ----------------- Classes -------------------
 // Each data element that we store in memory
 class Node 
 {
@@ -34,22 +35,9 @@ class Slot
             listhead = newnode;
         }
 };
+// ----------------- End Classes -------------------
 
-// Hashes the key given to a new key
-int hash_function(int key)
-{
-    return key % 100;
-}
-
-// It creates 100 slots from 0 to 99
-void create_slots(Slot slots[])
-{
-    for (int i = 0; i < 100; i++)
-    {
-        slots[i].key = i;
-    }
-}
-
+// ---------------- Data structure functions ---------------------
 // This function creates a new single node
 Node* create_node(int key, int data)
 {
@@ -57,14 +45,20 @@ Node* create_node(int key, int data)
     return temp;
 }
 
-// Methods
+// Hashes the key given to a new key
+int hash_function(int key)
+{
+    return key % 100;
+}
+
 // Inserting
-void insert(Slot slots[], int key, int data)
+void insertNode(Slot slots[], int key, int data)
 {
     int hashedkey = hash_function(key);
     slots[hashedkey].insert(create_node(key, data));
 }
 
+// Deleting
 void deleteNode(Node* head, Node* n)  
 {  
     // When node to be deleted is head node  
@@ -74,42 +68,32 @@ void deleteNode(Node* head, Node* n)
         {  
             return;  
         }  
-  
         /* Copy the data of next node to head */
         head->key = head->next->key;  
-  
         // store address of next node  
         n = head->next;  
-  
         // Remove the link of next node  
         head->next = head->next->next;  
-  
         // free memory  
         delete n;  
-  
         return;  
     }  
-  
     Node* prev = head;  
     while(prev->next != NULL && prev->next != n)  
         prev = prev->next;  
-  
     // Check if node really exists in Linked List  
     if(prev->next == NULL)  
     {  
         return;  
     }  
-  
     // Remove node from Linked List  
     prev->next = prev->next->next;  
-  
     // Free memory  
     delete n;
-  
     return;  
 }
 
-// Deleting
+// Searching and removing
 void remove(Slot slots[], int key)
 {
     int hashedkey = hash_function(key);
@@ -124,15 +108,19 @@ void remove(Slot slots[], int key)
     deleteNode(slots[hashedkey].listhead, head);
 }
 
+// It creates 100 slots from 0 to 99
+void create_slots(Slot slots[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        slots[i].key = i;
+    }
+}
+
+// Testbench
 int main()
 {
-    // Create an array of pointer as slots
-    // Create a table to generate a key
-    // Create a linked list for any slots to point at
-    // Add the methods
-    // Create the diagram of data structure
     Slot slots[100];
     create_slots(slots);
-    insert(slots, 10, 500);
     return 0;
 }
