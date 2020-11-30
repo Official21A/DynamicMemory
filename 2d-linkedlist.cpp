a79fbf79859f9b00a9a4dfc3c6f37500d4ad1fe1
Official21A <officialamirhossein21@gmail.com>
2020-11-30 08:14:23 +0330

    2dll head

--- 2d-linkedlist.cpp
+++ 2d-linkedlist.cpp
@@ -17,6 +17,57 @@ class Node
         };
 };
 
+Node* create_node(int value)
+{
+    return new Node(value);
+}
+
+class Head
+{
+    public:
+        int size;
+        Node* head;
+        Head()
+        {
+            size = 0;
+            head = NULL;
+        };
+        void add_to_head(Node* temp)
+        {
+            temp->next = head;
+            head = temp;
+        };
+        void remove(Node* temp)
+        {
+            Node* prev = NULL; 
+            
+            if (head != NULL && head == temp) 
+            { 
+                head = temp->next; 
+                delete temp;
+                return; 
+            } 
+
+            while (head != NULL && head != temp) 
+            { 
+                prev = head; 
+                head = head->next; 
+            } 
+        
+            if (head == NULL) 
+                return; 
+         
+            prev->next = head->next; 
+            delete temp; 
+        };
+};
+
+void insert(int value)
+{
+    Node* temp = create_node(value);
+
+}
+
 
 
 int main()
