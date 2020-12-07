cc911ae7272c58db07af7c768c8f8ca82a5ff62e
Official21A <officialamirhossein21@gmail.com>
2020-12-07 21:54:30 +0330

    add event

--- piority-queue.cpp
+++ piority-queue.cpp
@@ -23,9 +23,42 @@ Node* create_node(int key, string value)
     return new Node(key, value);
 }
 
+// Structure needes
+int memory_space = 100;
+int index = 0;
+Node* memory[memory_space];
+
+// Check the new adding
+void check_to_root(int index)
+{
+    int parent = int(index / 2);
+    while(parent != -1 && memory[parent] > memory[index])
+    {
+        Node* temp = memory[parent];
+        memory[parent] = memory[index];
+        memory[index] = temp;
+        index = parent;
+        parent = int(index / 2);
+    }
+}
+
+// Add a new event
+void add_event(int key, string value)
+{
+    if (index == memory_space)
+    {
+        cout << "Overflow" << endl;
+        return;
+    }
+    memory[index] = create_node(key, value);
+    check_to_root(index);
+    index++;
+}
+
+
 
 // Testbench
 int main()
 {
-    return 0;
+    
 }
